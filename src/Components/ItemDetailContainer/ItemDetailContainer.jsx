7456da3271aa86d1f0cecd131ed36cd7cc358383
Enzo <feitoenzo@gmail.com>
2023-03-24 06:57:02 -0300

    Entrega Termianda

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-
 import { useParams } from "react-router-dom";
-
 import { products } from "../../productsMock";
 import ItemCount from "../ItemCount/ItemCount";
+import MediaCard from "../MediaCard/MediaCard";
 
 const ItemDetailContainer = () => {
   const { id } = useParams();
 
   const productSelected = products.find((element) => element.id === Number(id));
 
-  const onAdd = (cantidad)=>{
-    console.log(`se agrego al carrito ${cantidad} productos `)
-  }
+  const onAdd = (cantidad) => {
+    console.log(`se agrego al carrito ${cantidad} productos `);
+  };
 
   return (
     <div>
-      <h1>{productSelected.title}</h1>
-      <img src={productSelected.img} alt="" />
+      <div>
+        <img src={productSelected.image} alt="" />
+      </div>
+      <MediaCard />
       <ItemCount stock={productSelected.stock} onAdd={onAdd} />
     </div>
   );
